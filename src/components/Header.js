1cec63ae7a3874520af0bd1b46260391dbe71509
Mariame EL ALAOUI <mariame.elalaoui@gmail.com>
2022-08-01 11:24:26 +0100

    modify header v0

--- src/components/Header.js
+++ src/components/Header.js
@@ -1,21 +1,21 @@
 import Link from 'next/link'
-
 const Header = () => {
   return (
-    <div className="bg-blue-500 p-2 shadow-lg">
-      <div className="mr-auto flex items-center bg-amber-400">
-        <Link href="/" className="text-decoration-none text-black h3 my-2">
-          Mariame
-        </Link>
+    <div className="d-flex bg-sky-100 p-2 shadow-lg justify-content-start align-items-center">
+      <div className="mr-auto flex items-center rgba(14,16,119,255)">
+        <div className="d-flex text-slate-50 font-bold tracking-wide h2 my-2">
+          ONDA Tétouan - CNS Indicator -
+        </div>
       </div>
-      <div>
-        <Link href="login">
-          <button className="btn mr-2">Se connecter</button>
-        </Link>
-        <Link href="register">
-          <button className="btn ">S&apos;inscrire</button>
-        </Link>
-      </div>
+
+      <Link href="login">
+        <button className="d-flex text-slate-50  border border-slate-50/40 mx-5 py-1 pl-2 pr-2 font-semibold shadow-2xl btn mr-2 rounded-lg ">
+          Se connecter
+        </button>
+      </Link>
+      <button className="d-flex text-red-800  border border-red-800 mx-2 py-1 pl-2 pr-2 font-semibold shadow-2xl btn mr-2 rounded-lg ">
+        Se Déconnecter
+      </button>
     </div>
   )
 }
--- tailwind.config.js
+++ tailwind.config.js
@@ -6,18 +6,33 @@ module.exports = {
     extend: {
       colors: {
         primary: '#5c6ac4',
-        secondary: '#ecc94b'
+        secondary: '#ecc94b',
+        white: '#fafafa',
+        sky: {
+          50: '#f0f9ff',
+          100: '#0e1077',
+          200: '#bae6fd',
+          300: '#7dd3fc',
+          400: '#38bdf8',
+          500: '#0ea5e9',
+          600: '#0284c7',
+          700: '#0369a1',
+          800: '#075985',
+          900: '#0c4a6e'
+        }
       }
     }
-  },
-  variants: {
-    extend: {
-      backgroundColor: ['active']
-    }
-  },
-  plugins: [
-    require('@tailwindcss/forms')({
-      strategy: 'class'
-    })
-  ]
+  }
 }
+//   },
+//   variants: {
+//     extend: {
+//       backgroundColor: ['active']
+//     }
+//   },
+//   plugins: [
+//     require('@tailwindcss/forms')({
+//       strategy: 'class'
+//     })
+//   ]
+// }
