7911ee148d6509665dcd639c7f57b93b021ad2c5
Mariame EL ALAOUI <mariame.elalaoui@gmail.com>
2022-08-03 12:37:03 +0100

    dd

--- src/components/Sidebar.js
+++ src/components/Sidebar.js
@@ -28,7 +28,7 @@ const Sidebar = () => {
            border-2 rounded-full  ${!open && 'rotate-180'}`}
           onClick={() => setOpen(!open)}
         />
-        <div className="flex flex-col items-center justify-center"></div>
+        {/* <div className="flex flex-col items-center justify-center"></div>
         <ul className="pt-6">
           {Menus.map((Menu, index) => (
             <li
@@ -36,14 +36,20 @@ const Sidebar = () => {
               className={`flex  rounded-md p-2 cursor-pointer hover:bg-light-white text-gray-300 text-sm items-center gap-x-4 
               ${Menu.gap ? 'mt-9' : 'mt-2'} ${index === 0 && 'bg-light-white'} `}
             >
-              <Image src={`src/components/assets/${Menu.src}.png`} alt="/" />
+              <Image
+                src={`/src/components/assets/${Menu.src}.png`}
+                layout="fill"
+                className={`absolute cursor-pointer -right-3 top-9 w-7 border-dark-purple
+                border-2 rounded-full  ${!open && 'rotate-180'}`}
+                alt="/"
+              />
               <span className={`${!open && 'hidden'} origin-left duration-200`}>{Menu.title}</span>
             </li>
           ))}
-        </ul>
+        </ul>*/}
       </div>
       <div className="h-screen flex-1 p-7">
-        <h1 className="text-2xl font-semibold ">Home Page</h1>
+        {/* <h1 className="text-2xl font-semibold "></h1> */}
       </div>
     </div>
   )
