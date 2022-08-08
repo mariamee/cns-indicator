0f41ed0db73d2add1264f9aef1bbfb33d930620c
Mariame EL ALAOUI <mariame.elalaoui@gmail.com>
2022-08-08 13:22:56 +0100

    modif 1-0

--- src/components/Sidebar.js
+++ src/components/Sidebar.js
@@ -5,13 +5,11 @@ const Sidebar = () => {
   const [open, setOpen] = useState(true)
   const Menus = [
     { title: 'Mon équipe', src: 'group' },
-    { title: 'Inbox', src: 'Chat' },
-    { title: 'Accounts', src: 'User', gap: true },
-    { title: 'Schedule ', src: 'Calendar' },
-    { title: 'Search', src: 'Search' },
-    { title: 'Analytics', src: 'Chart' },
-    { title: 'Files ', src: 'Folder', gap: true },
-    { title: 'Setting', src: 'Setting' }
+    { title: 'Mes équipements', src: 'Chat' },
+    { title: 'Indicateur', src: 'User', gap: true },
+    { title: 'Maintenance', src: 'Setting' },
+    { title: 'Cartographies', src: 'Search' },
+    { title: 'Bilans', src: 'Chart' }
   ]
 
   return (
@@ -28,7 +26,7 @@ const Sidebar = () => {
            border-2 rounded-full  ${!open && 'rotate-180'}`}
           onClick={() => setOpen(!open)}
         />
-        {/* <div className="flex flex-col items-center justify-center"></div>
+        <div className="flex flex-col items-center justify-center"></div>
         <ul className="pt-6">
           {Menus.map((Menu, index) => (
             <li
@@ -36,20 +34,20 @@ const Sidebar = () => {
               className={`flex  rounded-md p-2 cursor-pointer hover:bg-light-white text-gray-300 text-sm items-center gap-x-4 
               ${Menu.gap ? 'mt-9' : 'mt-2'} ${index === 0 && 'bg-light-white'} `}
             >
-              <Image
+              {/* <Image
                 src={`/src/components/assets/${Menu.src}.png`}
                 layout="fill"
                 className={`absolute cursor-pointer -right-3 top-9 w-7 border-dark-purple
                 border-2 rounded-full  ${!open && 'rotate-180'}`}
                 alt="/"
-              />
+              /> */}
               <span className={`${!open && 'hidden'} origin-left duration-200`}>{Menu.title}</span>
             </li>
           ))}
-        </ul>*/}
+        </ul>
       </div>
       <div className="h-screen flex-1 p-7">
-        {/* <h1 className="text-2xl font-semibold "></h1> */}
+        <h1 className="text-2xl font-semibold "></h1>
       </div>
     </div>
   )
