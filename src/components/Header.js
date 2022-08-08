84068d2137ae1c56b0da2ca0e6778e682250b67f
Mariame EL ALAOUI <mariame.elalaoui@gmail.com>
2022-08-08 12:48:23 +0100

    v2 header

--- src/components/Header.js
+++ src/components/Header.js
@@ -3,7 +3,7 @@ import Image from 'next/image'
 import logoImage from '/public/logoImage.jpeg'
 const Header = () => {
   return (
-    <div className="d-flex bg-background p-2 shadow-2xl justify-between flex-shrink-0">
+    <ul className=" flex flex-col gap-2 lg:flex-row lg:items-center bg-background p-2 shadow-2xl justify-between flex-shrink-0">
       <Image src={logoImage} alt="/" width={120} height={80} />
       <div className="mr-auto flex items-center rgba(14,16,119,255)">
         <div className="d-flex text-sky-100/80 font-bold tracking-wide h2 my-2">ONDA Tétouan </div>
@@ -11,14 +11,17 @@ const Header = () => {
       </div>
 
       <Link href="login">
-        <button className="d-flex text-sky-100  border border-sky-100/40 mx-5 py-1 pl-2 pr-2 font-semibold shadow-2xl btn mr-2 rounded-lg ">
+        <button
+          className="d-flex text-sky-100  border border-sky-100/40 mx-5
+         py-1 pl-2 pr-2 font-semibold shadow-2xl btn mr-2 rounded-lg group-hover:opacity-25"
+        >
           Se connecter
         </button>
       </Link>
       <button className="d-flex text-red-800  border border-red-800 mx-2 py-1 pl-2 pr-2 font-semibold shadow-2xl btn mr-2 rounded-lg ">
         Se Déconnecter
       </button>
-    </div>
+    </ul>
   )
 }
 
---
+++ src/components/utils/utils.js
@@ -0,0 +1,17 @@
+export const CATEGORY_CLASS = {
+  bg: {
+    [CONNECT]: 'bg-indigo-500',
+    [DISCONNECT]: 'bg-green-500',
+    default: 'bg-pink-500'
+  },
+  shadow: {
+    [CONNECT]: 'shadow-indigo-500/30',
+    [DISCONNECT]: 'shadow-green-500/30',
+    default: 'shadow-pink-500/30'
+  },
+  hover: {
+    [CONNECT]: 'hover:bg-indigo-500/5',
+    [DISCONNECT]: 'hover:bg-green-500/5',
+    default: 'hover:bg-pink-500/5'
+  }
+}
